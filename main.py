1d62a97d90b4a8a5f275d805a8bb475a3ccd1752
bsyzch <47052601+bsyzch@users.noreply.github.com>
2021-04-11 05:22:04 +0900

    Update 2 on GitHun Branch Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Update on GitHub branch")
 
 #adding new item in master branch
 print("Hello World Updates on Local Master Branch")
+
+#Updates in main branch of GitHub
+print("Hello World 2 Update on GitHub branch Only")
